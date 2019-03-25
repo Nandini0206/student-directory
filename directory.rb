f6c6dc6ad1c1a77056ebafe4f6c74a56026c1446
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-03-25 13:39:34 +0000

    Taking arguments from the command line

--- directory.rb
+++ directory.rb
@@ -21,7 +21,7 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, hit return twice"
 
-name = gets.chomp
+name = STDIN.gets.chomp
 
 while !name.empty? do
   @students << {name: name, cohort: :november}
@@ -30,7 +30,7 @@ while !name.empty? do
     else
       puts "Now we have #{@students.count} students"
     end
-  name = gets.chomp
+  name = STDIN.gets.chomp
   end
 end
 
@@ -84,7 +84,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -109,6 +109,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first# first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
 
-
-return interactive_menu
+try_load_students
+interactive_menu
